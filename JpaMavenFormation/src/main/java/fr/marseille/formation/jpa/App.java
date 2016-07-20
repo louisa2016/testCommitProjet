4edf25ebe1c45fd2db07fa7572d103f6559d52b7
soraya <saci_soraya@yahoo.fr>
2016-07-20 10:08:51 +0200

    commentaire dans la classe app.java

--- JpaMavenFormation/src/main/java/fr/marseille/formation/jpa/App.java
+++ JpaMavenFormation/src/main/java/fr/marseille/formation/jpa/App.java
@@ -2,12 +2,10 @@ package fr.marseille.formation.jpa;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // coucou louisa
     }
 }
